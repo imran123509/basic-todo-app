d80f5745384e5ec9e6f518c64c306eefd4891031
imran123509 <hasanimran114@gmail.com>
2024-01-22 17:52:05 +0530

    added somecode for bakend

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,36 @@
 const express=require('express');
-
+const {createTodo}=require('./types');
 const app=express();
 
 app.use(express.json());
 
 app.post("/todo", (req, res)=>{
+     const createPayload=req.body;
+     const parseedPayload=createTodo.safeParse(createPayload);
+     if(!parseedPayload.success){
+        res.status(411).json({
+            msg:"u send wrong input",
+        });
+
+
+     }
 
 });
 
 app.get("todos", (req, res)=>{
-
+    
 });
 
 app.put("/completed", (req, res)=>{
-      
+      const updatePayload=req.body;
+      const parseedPayload=updatePayload.safeParse(updatePayload);
+      if(!parseedPayload.success){
+         res.status(411).json({
+             msg:"u send wrong input",
+         });
+ 
+ 
+      }
 })
 
 app.listen(3000, ()=>{
